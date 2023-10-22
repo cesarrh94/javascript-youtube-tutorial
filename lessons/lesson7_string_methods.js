819c303a273c580fb56a945f3b4bd83c40b275de
cesar rodriguez <94cesarrh@gmail.com>
2023-10-21 23:01:58 -0600

    fix on indentation

--- lessons/lesson7_string_methods.js
+++ lessons/lesson7_string_methods.js
@@ -3,11 +3,8 @@
 let userName = "Cirus D Punk";
 
 console.log("userName string length: ", userName.length);
-console.log("Getting the first character from userName: ", userName.charAt(0));
-console.log(
-  "Getting the position of any letter on userName: ",
-  userName.indexOf("i")
-);
+console.log("Get the first character from userName: ", userName.charAt(0));
+console.log("Get position of any letter on userName: ", userName.indexOf("i"));
 
 console.log("userName in UpperCase: ", userName.toUpperCase());
 console.log("userName in LowerCase: ", userName.toLowerCase());
